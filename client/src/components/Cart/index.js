cb6d2a73365c83b20a9b9e71e8b4233e8c0c2030
Debbie Neflas <debbieneflas@gmail.com>
2022-09-28 17:24:43 -0700

    update dependecy versions and integrate stripe api

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,13 +1,19 @@
 import React, { useEffect } from "react";
+import { useLazyQuery } from "@apollo/client";
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
 import "./style.css";
 import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from "../../utils/queries";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
     async function getCart() {
@@ -22,6 +28,14 @@ const Cart = () => {
       getCart();
     }
   }, [state.cart.length, dispatch]);
+
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
 
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
@@ -45,6 +59,20 @@ const Cart = () => {
     );
   }
 
+  function submitCheckout() {
+    const productIds = [];
+
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+
+      getCheckout({
+        variables: { products: productIds },
+      });
+    });
+  }
+
   return (
     <div className="cart">
       <div className="close" onClick={toggleCart}>
@@ -59,7 +87,7 @@ const Cart = () => {
           <div className="flex-row space-between">
             <strong>Total: ${calculateTotal()}</strong>
             {Auth.loggedIn() ? (
-              <button>Checkout</button>
+              <button onClick={submitCheckout}>Checkout</button>
             ) : (
               <span>(log in to check out)</span>
             )}
